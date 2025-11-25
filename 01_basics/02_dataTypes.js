e8482f50140f7b82d0658f3bb66aa1431fc9e127
Rajnish Pradhan <rajnishpradhan@Rajnishs-MacBook-Pro.local>
2025-11-25 06:05:58 +0545

    data types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,6 +1,33 @@
-let isLoggedIn;
+let x = 3;
+const y = x++;
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
 
-console.log(isLoggedIn);
-console.log(typeof booleanIsLoggedIn);
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+console.log(x);
+
+const id = Symbol(123);
+const accountId = Symbol(123);
+
+console.log(id === accountId); // false
+console.log(typeof id);
+
+const heros = ["Spiderman", "Ironman", "Hulk"];
+let myObjj = {
+    name: "Thor",
+    age: 1500,
+    city: "Asgard"  
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+}
+console.log(typeof myFunction);
+
+    
